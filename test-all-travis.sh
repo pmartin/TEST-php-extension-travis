2061297edac95956a02d3ac48b9fc62f85651c61
Pascal MARTIN <contact@pascal-martin.fr>
2014-02-17 20:03:07 +0100

    first try for travis

---
+++ test-all-travis.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+
+export CFLAGS="-Wall -Wextra -Wdeclaration-after-statement -Wvla -Wmissing-field-initializers -Wshadow -Wno-unused-parameter"
+
+for dir in `ls -d -1 */`
+do
+    echo ">> Testing $dir"
+    cd $dir
+    
+    # Build
+    phpize
+    ./configure --enable-monext
+    make
+    
+    # Install
+    echo 'extension = monext.so' >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+    sudo make install
+    
+    # Launch tests
+    make test
+    
+    cd ..
+done
